52a238f76d0feb1e89feeec9e0d29d6c72252b11
shenKongTech <121651325+shenKongTech@users.noreply.github.com>
2025-01-20 06:33:00 +0000

    更新 事件与消息

---
+++ 事件与消息/匿名内部类实现按钮单击响应.java
@@ -0,0 +1,8 @@
+btn1.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Toast.makeText(MainActivity.this,"你单击了此按钮",Toast.LENGTH_SHORT).show();
+			}
+		});
